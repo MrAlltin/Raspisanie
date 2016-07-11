de6a03ca49a2a50db2c1ab2ce41ce27888123ee3
MrAlltin <amillboy@yandex.ru>
2016-07-12 04:21:22 +0500

    Improvements

--- app/src/main/java/ru/mydomain/raspisanieusatu/fragment/HistoryFragment.java
+++ app/src/main/java/ru/mydomain/raspisanieusatu/fragment/HistoryFragment.java
@@ -3,16 +3,23 @@ package ru.mydomain.raspisanieusatu.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.mydomain.raspisanieusatu.R;
+import ru.mydomain.raspisanieusatu.adapter.RemindListAdapter;
+import ru.mydomain.raspisanieusatu.dto.RemindDTO;
 
 
 public class HistoryFragment extends AbstractTabFragment{
 
-    private static final int LAYOUT = R.layout.fragment_example;
+    private static final int LAYOUT = R.layout.fragment_history;
 
 
     public static HistoryFragment getInstanse(Context context){
@@ -30,8 +37,25 @@ public class HistoryFragment extends AbstractTabFragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT,container,false);
 
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.recycleView);
+        rv.setLayoutManager(new LinearLayoutManager(context));
+        rv.setAdapter(new RemindListAdapter(creatMockRemindListData()));
+
 
         return view;
+    }
+
+    private List<RemindDTO> creatMockRemindListData() {
+        List<RemindDTO> data = new ArrayList<>();
+        data.add(new RemindDTO("Item 1"));
+        data.add(new RemindDTO("Item 2"));
+        data.add(new RemindDTO("Item 3"));
+        data.add(new RemindDTO("Item 4"));
+        data.add(new RemindDTO("Item 5"));
+        data.add(new RemindDTO("Item 6"));
+
+
+        return data;
     }
 
     public void setContext(Context context) {
