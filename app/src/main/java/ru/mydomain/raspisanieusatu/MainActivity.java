52c5d3e1c05ef341f558ca3a63f2de70c3ba75b2
MrAlltin <amillboy@yandex.ru>
2016-07-12 02:12:47 +0500

    TabNavigator

--- app/src/main/java/ru/mydomain/raspisanieusatu/MainActivity.java
+++ app/src/main/java/ru/mydomain/raspisanieusatu/MainActivity.java
@@ -1,9 +1,11 @@
 package ru.mydomain.raspisanieusatu;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -63,6 +65,29 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigatonView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open,R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNatificationTab();
+                }
+
+                return true;
+            }
+        });
+
+    }
+
+    private void showNatificationTab(){
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
 
