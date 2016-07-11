5a7338d6f95591e2638af6d9622ee492f99a296c
MrAlltin <amillboy@yandex.ru>
2016-07-12 01:17:04 +0500

    Items on Navigation menu

--- app/src/main/java/ru/mydomain/raspisanieusatu/MainActivity.java
+++ app/src/main/java/ru/mydomain/raspisanieusatu/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.mydomain.raspisanieusatu;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -9,16 +10,22 @@ import android.view.MenuItem;
  * Created by TotAll on 11.07.2016.
  */
 public class MainActivity extends AppCompatActivity {
+    private static final int LAYOUT = R.layout.activity_main;
+
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigatonView();
     }
+
 
     private void initToolbar() {
         toolbar = (Toolbar)findViewById(R.id.toolbar);
@@ -31,4 +38,12 @@ public class MainActivity extends AppCompatActivity {
         });
         toolbar.inflateMenu(R.menu.menu);
     }
+
+
+
+    private void initNavigatonView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
 }
+
+
